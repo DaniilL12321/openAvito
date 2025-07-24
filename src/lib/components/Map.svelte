1ef346272672faddb889500d1b3bd67c6a1cd7db
DaniilL12321 <lobanov006@gmail.com>
2025-07-24 16:40:50 +0300

    adapt theme card for user selected theme site

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -10,27 +10,61 @@
   let map: mapboxgl.Map;
   let mapContainer: HTMLDivElement;
 
+  const createMarkerElement = () => {
+    const el = document.createElement('div');
+    el.className = 'custom-marker';
+    return el;
+  };
+
+  const isDarkTheme = () => document.documentElement.classList.contains('dark');
+
+  const updateMapStyle = () => {
+    if (map) {
+      map.setStyle(isDarkTheme() 
+        ? 'mapbox://styles/mapbox/dark-v11'
+        : 'mapbox://styles/mapbox/streets-v12'
+      );
+    }
+  };
+
   onMount(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
-
+    
     map = new mapboxgl.Map({
       container: mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: isDarkTheme() 
+        ? 'mapbox://styles/mapbox/dark-v11'
+        : 'mapbox://styles/mapbox/streets-v12',
       center: [lon, lat],
       zoom: zoom
     });
 
-    new mapboxgl.Marker()
+    new mapboxgl.Marker({
+      element: createMarkerElement()
+    })
       .setLngLat([lon, lat])
       .addTo(map);
 
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-  });
 
-  onDestroy(() => {
-    if (map) {
-      map.remove();
-    }
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'class') {
+          updateMapStyle();
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true
+    });
+
+    onDestroy(() => {
+      if (map) {
+        observer.disconnect();
+        map.remove();
+      }
+    });
   });
 </script>
 
@@ -45,5 +79,13 @@
   }
   :global(.mapboxgl-ctrl-bottom-right) {
     display: none !important;
+  }
+  :global(.custom-marker) {
+    width: 24px;
+    height: 24px;
+    background-color: hsl(var(--primary));
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: 0 0 0 4px rgba(0,0,0,0.1);
   }
 </style> 
